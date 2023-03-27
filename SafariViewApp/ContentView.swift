63b7b2cc5cdf0892791fc1ba47e2340fcba816c3
Simon Bestler <68077359+bestler@users.noreply.github.com>
2023-03-27 10:32:00 +0200

    Added UI to pass in url

--- SafariViewApp/ContentView.swift
+++ SafariViewApp/ContentView.swift
@@ -9,28 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State private var isShowSafari: Bool = false
+    @State private var link: String = ""
+    @State private var url: URL?
 
     private var dismissButtonStyle = SFSafariView.DismissButtonStyle.cancel
 
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Link to open in SafariView", text: $link)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .autocorrectionDisabled()
+                .autocapitalization(.none)
+                .keyboardType(.URL)
+                .textContentType(.URL)
+                .padding()
+                .onSubmit(of: .text) {
+                    presentLink()
+                }
+            Button("Open") {
+                presentLink()
+            }
+            .buttonStyle(.borderedProminent)
+
         }
-        .onTapGesture {
-            isShowSafari.toggle()
+
+        .sheet(item: $url) { url in
+            SFSafariView(url: url)
         }
-        .sheet(isPresented: $isShowSafari, content: {
-            SFSafariView(url: URL(string: "https://apple.de")!)
-        })
+    }
+
+    private func presentLink() {
+        let newURL = URL(string: link)
+        if let newURL = newURL {
+            url = newURL
+        }else {
+            print("No url")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension URL: Identifiable {
+    public var id: Int {
+        return self.hashValue
     }
 }
