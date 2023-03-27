152fda4097b9266fe2f764f64e0663ebd6944101
Simon Bestler <68077359+bestler@users.noreply.github.com>
2023-03-27 12:26:39 +0200

    Parameterized SFSafariView

--- SafariViewApp/SFSafariViewWrapper.swift
+++ SafariViewApp/SFSafariViewWrapper.swift
@@ -12,16 +12,51 @@ import Foundation
 struct SFSafariView: UIViewControllerRepresentable {
 
     let url : URL
+    let dismissButtonStyle: DismissButtonStyle
+    let preferredBarTintColor: UIColor?
+    let preferredControlTintColor: UIColor?
+    let isBarCollapsing: Bool
+    let entersReaderIfAvailable: Bool
+
+    init(url: URL,
+         dismissButtonStyle: DismissButtonStyle = .done,
+         preferredBarTintColor : UIColor? = .systemBackground,
+         preferredControlTintColor : UIColor? = .tintColor,
+         isBarCollapsing: Bool = true,
+         entersReaderIfAvailable: Bool = false) {
+        self.url = url
+        self.dismissButtonStyle = dismissButtonStyle
+        self.preferredBarTintColor = preferredBarTintColor
+        self.preferredControlTintColor = preferredControlTintColor
+        self.isBarCollapsing = isBarCollapsing
+        self.entersReaderIfAvailable = entersReaderIfAvailable
+    }
 
     func makeUIViewController(context: Context) -> some UIViewController {
 
-        var configuration = SFSafariViewController.Configuration()
-        configuration.barCollapsingEnabled = true
-        var viewController = SFSafariViewController(url: url, configuration: configuration)
-        viewController.preferredBarTintColor = .systemBackground
+        let configuration = SFSafariViewController.Configuration()
+        configuration.barCollapsingEnabled = isBarCollapsing
+        configuration.entersReaderIfAvailable = entersReaderIfAvailable
+
+        let viewController = SFSafariViewController(url: url, configuration: configuration)
+
         viewController.modalPresentationStyle = UIModalPresentationStyle.currentContext
 
-        viewController.dismissButtonStyle = SFSafariViewController.DismissButtonStyle.cancel
+        //Setting the colors
+        viewController.preferredBarTintColor = preferredBarTintColor
+        viewController.preferredControlTintColor = preferredControlTintColor
+
+
+        // Configure dismissButtonStyle
+        switch dismissButtonStyle {
+        case .cancel:
+            viewController.dismissButtonStyle = SFSafariViewController.DismissButtonStyle.cancel
+        case .close:
+            viewController.dismissButtonStyle = SFSafariViewController.DismissButtonStyle.close
+        case .done:
+            viewController.dismissButtonStyle = SFSafariViewController.DismissButtonStyle.done
+        }
+
         return viewController
     }
 
